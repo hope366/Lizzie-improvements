b7914f5ecf09f639c7b2a069bf19315da353f22e
hope366 <63999713+hope366@users.noreply.github.com>
2020-10-19 23:23:56 +0900

    Fix empty dialogs on Linux

--- src/main/java/featurecat/lizzie/analysis/Leelaz.java
+++ src/main/java/featurecat/lizzie/analysis/Leelaz.java
@@ -128,15 +128,8 @@ public class Leelaz {
       // substitute in the weights file
       engineCommand = engineCommand.replaceAll("%network-file", config.getString("network-file"));
     }
-    Matcher nicknameMatcher = Pattern.compile("<([^<]*)>\\s*(.*)").matcher(engineCommand);
-    if (nicknameMatcher.matches()) {
-      engineNickname = nicknameMatcher.group(1);
-      engineCommand = nicknameMatcher.group(2);
-    } else {
-      engineNickname = null;
-    }
-
-    this.engineCommand = engineCommand;
+    updateEngineCommandAndNickname(engineCommand);
+    engineCommand = this.engineCommand;
     if (engineCommand.toLowerCase().contains("override-version")) {
       this.isKataGo = true;
     }
@@ -957,7 +950,8 @@ public class Leelaz {
   }
 
   public boolean isCommandChange(String command) {
-    List<String> newList = splitCommand(command);
+    updateEngineCommandAndNickname(command);
+    List<String> newList = splitCommand(engineCommand);
     if (this.commands.size() != newList.size()) {
       return true;
     } else {
@@ -970,6 +964,17 @@ public class Leelaz {
         }
       }
       return false;
+    }
+  }
+
+  private void updateEngineCommandAndNickname(String command) {
+    Matcher nicknameMatcher = Pattern.compile("<(.*?)>\\s*(.*)").matcher(command);
+    if (nicknameMatcher.matches()) {
+      engineNickname = nicknameMatcher.group(1);
+      engineCommand = nicknameMatcher.group(2);
+    } else {
+      engineNickname = null;
+      engineCommand = command;
     }
   }
 
