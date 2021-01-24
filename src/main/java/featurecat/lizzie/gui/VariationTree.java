32cd8bb0a3be5b4481ae8ab11a51e980baa71371
hope366 <63999713+hope366@users.noreply.github.com>
2021-01-25 00:48:30 +0900

    Fine-tune the position of the number

--- src/main/java/featurecat/lizzie/gui/VariationTree.java
+++ src/main/java/featurecat/lizzie/gui/VariationTree.java
@@ -148,7 +148,9 @@ public class VariationTree {
         g.setColor(Color.WHITE);
         int moveMNNumber = startNode.getData().moveMNNumber;
         g.drawString(
-            String.valueOf(moveMNNumber < 0 ? 0 : moveMNNumber),curposx + RING_DIAM,posy + RING_DIAM);
+            String.valueOf(moveMNNumber < 0 ? 0 : moveMNNumber),
+            curposx + RING_DIAM,
+            (posy - 4) + RING_DIAM);
       }
       g.setColor(curcolor);
     }
@@ -203,7 +205,9 @@ public class VariationTree {
                 + (Lizzie.config.nodeColorMode == 0 ? 1 : 0));
         g.setColor(Color.WHITE);
         g.drawString(
-            String.valueOf(cur.getData().moveMNNumber),curposx + RING_DIAM,posy + RING_DIAM);
+            String.valueOf(cur.getData().moveMNNumber),
+            curposx + RING_DIAM,
+            (posy - 5) + RING_DIAM);
       }
     }
     // Now we have drawn all the nodes in this variation, and has reached the bottom of this
